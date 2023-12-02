4209d97b321eafaf364f95c0cdee756bfefc13be
Md Sagar Miah <sagar.sa2065@gmail.com>
2023-12-02 22:37:25 +0600

    first commit

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,10 +1,7 @@
-"use client"
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const navbar = () => {
-    const Current = usePathname();
     return (
         <section className=' py-8'>
             <div className='container mx-8 xl:mx-auto max-w-6xl flex  justify-between'>
@@ -12,11 +9,11 @@ const navbar = () => {
                     Design<span className=' text-orange-500 '>AGENCY</span>
                 </div>
                 <div className='flex flex-nowrap gap-4 pt-1'>
-                    <Link className={Current === "/" ? " text-green-600 text-sm font-semibold   px-5 py-2 " : "hover:text-green-600 text-sm font-semibold   px-5 py-2 "} href="/">Home</Link>
-                    <Link className={Current === "/team" ? " text-green-600 text-sm font-semibold   px-5 py-2 " : "hover:text-green-600 text-sm font-semibold   px-5 py-2 "} href="/team">Team</Link>
-                    <Link className={Current === "/service" ? " text-green-600 text-sm font-semibold   px-5 py-2 " : "hover:text-green-600 text-sm font-semibold   px-5 py-2 "} href="/service">Service</Link>
-                    <Link className={Current === "/projects" ? " text-green-600 text-sm font-semibold   px-5 py-2 " : "hover:text-green-600 text-sm font-semibold   px-5 py-2 "} href="/projects">Projects</Link>
-                    <Link className={Current === "/testimonials" ? " text-green-600 text-sm font-semibold   px-5 py-2 " : "hover:text-green-600 text-sm font-semibold   px-5 py-2 "} href="/testimonials">Testimonials</Link>
+                    <Link className=' text-sm font-semibold hover:text-green-600  px-5 py-2 ' href="/">Home</Link>
+                    <Link className=' text-sm font-semibold hover:text-green-600  px-5 py-2 ' href="/team">Team</Link>
+                    <Link className=' text-sm font-semibold hover:text-green-600  px-5 py-2 ' href="/service">Service</Link>
+                    <Link className=' text-sm font-semibold hover:text-green-600  px-5 py-2 ' href="/projects">Projects</Link>
+                    <Link className=' text-sm font-semibold hover:text-green-600 px-5 py-2 ' href="/testimonials">Testimonials</Link>
                     <Link className=' text-sm font-semibold border border-solid border-green-600 rounded-lg px-5 py-2 hover:bg-white hover:ring-2 hover:ring-green-600 hover:text-green-600 ' href="/">Login</Link>
                     <Link className=' text-sm text-white font-semibold border border-solid bg-green-600 rounded-lg px-5 py-2 hover:bg-white hover:ring-2 hover:ring-green-600 hover:text-green-600 ' href="/">Register</Link>
                 </div>
